fc42a84d6007e82a0985fee740a033b99f5323a5
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-09-06 15:56:46 -0300

    Gerar número aleatório

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+let listNumber = [];
+
 function random(){
     let quantity = document.getElementById('quantity').value;
     let from = document.getElementById('from').value;
     let to = document.getElementById('to').value;
+
+    generateAleatoryNumber();
+
+}
+
+function generateAleatoryNumber(){
+    let chosenNumber = parseInt(Math.random() * to + 1);
+
+    for (let i = 0; i < quantity; i++){
+        listNumber.push(chosenNumber);
+    }
 }
