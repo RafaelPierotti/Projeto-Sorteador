b47bef0ee5fbeea430dfee148169b992cc2c54fd
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-09-06 15:39:37 -0300

    Sortear números

---
+++ app.js
@@ -0,0 +1,5 @@
+function random(){
+    let quantity = document.getElementById('quantity').value;
+    let from = document.getElementById('from').value;
+    let to = document.getElementById('to').value;
+}
